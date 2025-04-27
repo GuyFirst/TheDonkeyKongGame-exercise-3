6d144cecb9c6e0d45da091b3791be0e9d0006891
Guy First <guyfi2@mta.ac.il>
2025-04-27 22:34:03 +0300

    Update Entity.h

--- TheDonkeyKongGame/Entity.h
+++ TheDonkeyKongGame/Entity.h
@@ -13,6 +13,7 @@ protected:
         CLIMBING_DOWN,
         FALLING
     };
+
     Point startingPosition;
     Point position;
     char me;
@@ -25,7 +26,7 @@ protected:
     virtual void downLadder();
     virtual void followMario();
     bool isNearBoundry() const;
-    bool isOnFloor() const;
+    bool isOnFloor()     const;
     bool isUnderLadder() const {
         return map->currentMap[position.getY() - 1][position.getX()] == 'H' ||
             map->currentMap[position.getY()][position.getX()] == 'H' ||
@@ -36,16 +37,15 @@ protected:
         return map->currentMap[position.getY() + 2][position.getX()] == 'H' ||
             map->currentMap[position.getY() + 1][position.getX()] == 'H';
     }
+
 public:
     Entity(Point startingPosition, char me, int m_diff_x, int m_diff_y, Map* map)
         : startingPosition(startingPosition), position(startingPosition), me(me), m_diff_x(m_diff_x), m_diff_y(m_diff_y), map(map) {
     }
 
     virtual ~Entity() = default;
-
     virtual void draw(char ch, bool isLoad, bool isSave, bool isSilent) const;
     
-
     Point getPoint() const { return position; }
     int getX() const { return position.getX(); }
     int getY() const { return position.getY(); }
