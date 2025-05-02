37def3f11c1187712dc8bde2b0a033d506e55387
Guy First <guyfi2@mta.ac.il>
2025-05-02 05:19:41 +0300

    Update Game.cpp

--- TheDonkeyKongGame/Game.cpp
+++ TheDonkeyKongGame/Game.cpp
@@ -30,25 +30,28 @@ void Game::run()
  
         int flag = 0;
         Point screenStart(0, 0);
+	
         if (_isSilent) {
             std::cout << "running in silent mode, please wait";
             Sleep((int)gameConfig::Sleep::SCREEN_SLEEP);
         }
-        while (true)                                        //
-        {                                                   //
-            gotoxy(screenStart.getX(), screenStart.getY()); // 
+	
+        while (true)                                        
+        {                                                   
+            gotoxy(screenStart.getX(), screenStart.getY());  
             Map menu;                                       // the flags indicates if the player wins, loses, or quiting the game.
             if (typeid(*this) == typeid(GameSave))
             {
                 flag = menu.mainMenu(vec_to_fill);                         //  -1 for deciding to quit (before we entered the game)
-                if (flag == -1)                                 //   1 if the player won
+                if (flag == -1)                                            //   1 if the player won
                     return;
                 if (vec_to_fill.size() == 0)
                 {
                     noScreensMessage();
                     return;
                 }
-                flag = startGame(vec_to_fill, flag - '1');      //  -1 if the player lost
+		    
+                flag = startGame(vec_to_fill, flag - '1');                   //  -1 if the player lost
                 if (flag == 1)                                 
                     win();                                     
                 else                                            
