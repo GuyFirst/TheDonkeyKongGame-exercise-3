a4a0b43447ef6cd5bb875a9a9e535572dfd6323b
Guy First <guyfi2@mta.ac.il>
2025-05-04 15:47:56 +0300

    Update Ghost.cpp

--- TheDonkeyKongGame/Ghost.cpp
+++ TheDonkeyKongGame/Ghost.cpp
@@ -7,19 +7,6 @@
 #include "typeinfo"
 #include "UniqueGhost.h"
 
-
-//ok here me out
-// i want all the ghosts to reach the move function.
-// 
-// but before the handleCollision and handleDirectionChange, ill ask
-// if this is a regular ghost.
-// a regular ghost will go inside annd unique ghost will not.
-// in addition, need to implement some "follow mario" algorithm that only Unique ghost could follow.
-// and also climb the ladder...for that i gave the Entity the enum class for States to implement at the unique ghost
-// 
-//
-
-
 void Ghost::move(std::vector<std::unique_ptr<Ghost>>& ghosts, bool isLoad, bool isSave, bool isSilent) {
     draw(map->originalMap[position.getY()][position.getX()], isLoad, isSave, isSilent);
     
