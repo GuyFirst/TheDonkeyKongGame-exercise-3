424ef6a8da0f30e8df788418f280253fb5cced8d
Guy First <guyfi2@mta.ac.il>
2025-05-04 15:48:36 +0300

    Update Ghost.cpp

--- TheDonkeyKongGame/Ghost.cpp
+++ TheDonkeyKongGame/Ghost.cpp
@@ -7,32 +7,35 @@
 #include "typeinfo"
 #include "UniqueGhost.h"
 
-void Ghost::move(std::vector<std::unique_ptr<Ghost>>& ghosts, bool isLoad, bool isSave, bool isSilent) {
+void Ghost::move(std::vector<std::unique_ptr<Ghost>>& ghosts, bool isLoad, bool isSave, bool isSilent)
+{
     draw(map->originalMap[position.getY()][position.getX()], isLoad, isSave, isSilent);
     
         handleCollision(ghosts);
         handleDirectionChange();
-   
-      
-    // Update position 
-    if (isOnAir() || isNearBoundry()) {
+     
+    if (isOnAir() || isNearBoundry())
+    {
         m_diff_x = -m_diff_x;
     }
 
     position.setX(position.getX() + m_diff_x);
-    // Draw ghost at the new position
     draw(me, isLoad, isSave, isSilent);
 }
 
 
-void Ghost::handleCollision(std::vector<std::unique_ptr<Ghost>>& ghosts) {
-    for (auto& other : ghosts) {
+void Ghost::handleCollision(std::vector<std::unique_ptr<Ghost>>& ghosts)
+{
+    for (auto& other : ghosts)
+    {
         // Skip checking collision with itself
-        if (other.get() == this) {
+        if (other.get() == this)
+        {
             continue;
         }
 
-        if (this->isNearOtherGhosts(*other)) {
+        if (this->isNearOtherGhosts(*other))
+        {
             this->m_diff_x = -(this->m_diff_x);
             other->m_diff_x = -(other->m_diff_x);
         }
